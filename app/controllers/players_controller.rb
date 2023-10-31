f90be1322c108aa2613933ce93a387fc6becb53f
yosyncr <yizhig866@gmail.com>
2023-10-31 14:21:22 +0900

    追加完了

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,12 +1,20 @@
 class PlayersController < ApplicationController
 
   def index
-  @players = Player.all
+   @players = Player.all
   end
 
   def new
-    @tweet = Tweet.new
+   @player = Player.new
   end
 
-  
+  def create
+    Player.create(player_params)
+    redirect_to '/'
+  end
+
+  private
+  def player_params
+    params.require(:player).permit(:name)
+  end
 end
